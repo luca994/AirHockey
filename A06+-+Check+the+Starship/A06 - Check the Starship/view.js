4fd72db012144da339798c89580159a1798be9c9
andreataglia <drew.taglia@gmail.com>
2019-04-14 11:10:17 +0200

    ass6 done

---
+++ A06+-+Check+the+Starship/A06 - Check the Starship/view.js
@@ -0,0 +1,11 @@
+function view(cx, cy, cz, alpha, beta, rho) {
+	// Create a view matrix for a camera in position cx, cy and cz, looking in the direction specified by
+	// alpha, beta and rho, as outlined in the course slides.
+	//Mv = (Mc)^-1 = R z (− ρ )⋅ Rx (−β )⋅ Ry (− α )⋅ T (−c x , −c y , −c z )
+
+	var out = utils.multiplyMatrices(utils.MakeRotateYMatrix(-alpha),utils.MakeTranslateMatrix(-cx,-cy,-cz));
+	out = utils.multiplyMatrices(utils.MakeRotateXMatrix(-beta), out);
+	out = utils.multiplyMatrices(utils.MakeRotateZMatrix(-rho), out);			   
+
+	return out;
+}
