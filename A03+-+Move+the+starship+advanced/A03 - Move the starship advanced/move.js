efc4d4f3d69d65e3dada4e3963cea5f27a79f63e
andreataglia <drew.taglia@gmail.com>
2019-05-04 16:57:10 +0200

    ass3 fully done

--- A03+-+Move+the+starship+advanced/A03 - Move the starship advanced/move.js
+++ A03+-+Move+the+starship+advanced/A03 - Move the starship advanced/move.js
@@ -16,9 +16,11 @@ function move() {
 	S1 = utils.multiplyMatrices(utils.MakeTranslateMatrix(1,1,0), S1);
 
 	// Mirror the starship along a plane passing through (1,2,0), and obtained rotating 38 degree around the y axis the xy plane
-	var S2 = utils.multiplyMatrices(utils.MakeRotateXMatrix(45), utils.MakeScaleMatrix(2));
-	S2 = utils.multiplyMatrices(utils.MakeRotateYMatrix(30), S2);
-	S2 = utils.multiplyMatrices(utils.MakeTranslateMatrix(1,1,-2), S2);
+	// T(1,2,0)*Ry(38)*S(1,1,-1)*Ry(38)^*T(1,2,0)^
+	var S2 = utils.multiplyMatrices(utils.invertMatrix(utils.MakeRotateYMatrix(-76)), utils.invertMatrix(utils.MakeTranslateMatrix(1,2,0)));
+	S2 = utils.multiplyMatrices(utils.MakeScaleMatrix(1,1,-1), S2);
+	// S2 = utils.multiplyMatrices(utils.MakeRotateYMatrix(80), S2);
+	S2 = utils.multiplyMatrices(utils.MakeTranslateMatrix(1,2,0), S2);
 
 	// The ship has been doubled in size, rotated 45 degrees around the x axis, 30 degrees around the y axis, and moved to (1,1,-2). Return the ship in its original position
 	//(T(1,1,-2)*Ry(30)*Rx(45)*S(2))^-1
