e90d1f7caa142cfecd423ca3c49d9d9e667713b2
andreataglia <drew.taglia@gmail.com>
2019-05-19 11:32:43 +0200

    ass7 done

--- A07-+Around+the+world/A07- Around the world/World.js
+++ A07-+Around+the+world/A07- Around the world/World.js
@@ -3,22 +3,30 @@ var worldAngle = 0;
 var worldElevation = 0;
 var worldRoll = 0;
 
+//now with quaternions to overcome the gimble lock issue
+var currentQ = new Quaternion();
+
 // this function returns the world matrix with the updated rotations.
 // parameters rvx, rvy and rvz contains a value in the -1 .. +1 range that tells the angular velocity of the world.
 function updateWorld(rvx, rvy, rvz) {
-	// updates the angles
-	worldAngle += rvy;
-	worldElevation += rvx;
-	worldRoll += rvz;
+    // updates the angles
+    worldAngle += rvy;
+    worldElevation += rvx;
+    worldRoll += rvz;
 
-	theta = ||omega||*dt; //length of velocity vector
-	// compute the rotation matrix
-	var out = q.toMatrix4();
-	// var out =  utils.multiplyMatrices(utils.multiplyMatrices(
-	// 				utils.MakeRotateYMatrix(worldAngle),
-	// 				utils.MakeRotateXMatrix(worldElevation)),
-	// 				utils.MakeRotateZMatrix(worldRoll));			   
+    //update the quaternion
+    let qX = Quaternion.fromAxisAngle([1, 0, 0], rvx * Math.PI / 180);
+    let qY = Quaternion.fromAxisAngle([0, 1, 0], rvy * Math.PI / 180);
+    let qZ = Quaternion.fromAxisAngle([1, 0, 1], rvz * Math.PI / 180);
 
-	return out;
+    currentQ = qY.mul(qX.mul(qZ.mul(currentQ)));
+
+    // compute the rotation matrix
+    var out = currentQ.toMatrix4();
+    // var out =  utils.multiplyMatrices(utils.multiplyMatrices(
+    // 				utils.MakeRotateYMatrix(worldAngle),
+    // 				utils.MakeRotateXMatrix(worldElevation)),
+    // 				utils.MakeRotateZMatrix(worldRoll));			   
+
+    return out;
 }
-
