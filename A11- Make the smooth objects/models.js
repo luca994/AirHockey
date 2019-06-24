1f0f1b1eed7fdddb8a7ebeab9017d0e6de96b505
Luca Santini <luca2.santini@mail.polimi.it>
2019-06-24 23:50:32 +0200

    fixed smooth object assignment

--- A11- Make the smooth objects/models.js
+++ A11- Make the smooth objects/models.js
@@ -29,6 +29,8 @@ function buildGeometry() {
 		norm2[2*i] = [Math.cos(2*Math.PI / slices2 * i), -1.0, Math.sin(2*Math.PI / slices2 * i)];
 		vert2[2*i+1] = [Math.cos(2*Math.PI / slices2 * i), 1.0, Math.sin(2*Math.PI / slices2 * i)];
 		norm2[2*i+1] = [Math.cos(2*Math.PI / slices2 * i), 1.0, Math.sin(2*Math.PI / slices2 * i)];
+		norm2[2*i]  =  normalize(norm2[2*i]);
+		norm2[2*i+1] = normalize(norm2[2*i+1]);
 	}
 	j=0
 	// Lateral area
@@ -39,27 +41,43 @@ function buildGeometry() {
 	  	ind2[j+3] = (i < slices2-1 ) ? 2*i+2 : 0;
 	  	ind2[j+4] = 2*i+1;
 	  	ind2[j+5] = (i < slices2-1 ) ? 2*i+3 : 1;
-	  	j+=6
-	}
-	vert2[2*slices2] = [0.0,1.0,0.0]
-	norm2[2*slices2] = [0.0,1.0,0.0]
-	// Top area
-	for(i=1; i <= 2*slices2-1; i+=2){
-		ind2[j]   = 2*slices2;
-		ind2[j+1] = i;
-		ind2[j+2] = (2*slices2-2+i) % (2*slices2);
-		j+=3
-	}
-	vert2[2*slices2+1] = [0.0,-1.0,0.0]
-	norm2[2*slices2+1] = [0.0,-1.0,0.0]
-	// Bottom area
-	for(i=0; i <= slices2*2-2; i+=2){
-		ind2[j]   = 2*slices2+1;
-	  ind2[j+1] = i;
-	  ind2[j+2] = (i+2) % (2*slices2);
-	  j+=3
+	  	j+=6;
 	}
 
+	vert2[2*slices2] = [0.0,1.0,0.0];
+	norm2[2*slices2] = [0.0,1.0,0.0];
+	j = 2*slices2+1;
+	for(i = 0; i < slices2; i++) {
+		vert2[j] = [Math.cos(2*Math.PI / slices2 * i), 1.0, Math.sin(2*Math.PI / slices2 * i)];
+		norm2[j] = [0.0, 1.0, 0.0];
+		j++;
+	}
+	ckpt = j;
+	// Top area
+	j = 6*slices2;
+	for(i=0; i<slices2; i++){
+		ind2[j]   = 2*slices2;
+		ind2[j+1] = (i < slices2-1) ? 2*slices2+i+2 : 2*slices2+1;
+		ind2[j+2] = 2*slices2+i+1;
+		j+=3
+	}
+
+	vert2[ckpt] = [0.0,-1.0,0.0];
+	norm2[ckpt] = [0.0,-1.0,0.0];
+	j = ckpt+1;
+	for(i = 0; i < slices2; i++) {
+		vert2[j] = [Math.cos(2*Math.PI / slices2 * i), -1.0, Math.sin(2*Math.PI / slices2 * i)];
+		norm2[j] = [0.0, -1.0, 0.0];
+		j++;
+	}
+	// Bottom area
+	j = 9*slices2;
+	for(i=0; i<slices2; i++){
+		ind2[j]   = ckpt;
+		ind2[j+1] = ckpt+1+i;
+		ind2[j+2] = (i < slices2-1) ? ckpt+2+i : ckpt+1;
+		j+=3
+	}
 	addMesh(vert2, norm2, ind2, color2);
 
 
@@ -72,23 +90,30 @@ function buildGeometry() {
 	for(i = 0; i < slices3; i++) {
 		vert3[i+1] = [Math.cos(2*Math.PI / slices3 * i), -1.0, Math.sin(2*Math.PI / slices3 * i)];
 		norm3[i+1] = [Math.cos(2*Math.PI / slices3 * i), 0.0, Math.sin(2*Math.PI / slices3 * i)];
+		norm3[i+1] = normalize(norm3[i+1]);
 	}
 	// Lateral area
 	j = 0
-	for(i=slices3; i > 0; i--){
+	for(i=0; i < slices3; i++){
 		ind3[j]   = 0;
-		ind3[j+1] = i;
-		ind3[j+2] = (i > 1) ? i-1 : slices3;
+		ind3[j+1] = (i < slices3-1) ? i+2 : 1;
+		ind3[j+2] = i+1;
 	  	j+=3
 	}
 	vert3[slices3+1] = [0.0, -1.0, 0.0]
 	norm3[slices3+1] = [0.0, -1.0, 0.0]
+	j = slices3+1;
+	for(i = 0; i < slices3; i++) {
+		vert3[j] = [Math.cos(2*Math.PI / slices3 * i), -1.0, Math.sin(2*Math.PI / slices3 * i)];
+		norm3[j] = [0.0, -1.0, 0.0];
+		j++;
+	}
 	j = 0
-	// Bottm area
-	for(i = 1; i <= slices3; i++) {
+	// Bottom area
+	for(i = 0; i < slices3; i++) {
 		ind3[3*slices3+j]   = slices3+1;
-		ind3[3*slices3+j+1] = i;
-		ind3[3*slices3+j+2] = (i < slices3) ? i+1 : 1;
+		ind3[3*slices3+j+1] = slices3+i;
+		ind3[3*slices3+j+2] = (i < slices3-1) ? slices3+i+1 : 1;
 		j+=3
 	}
 	addMesh(vert3, norm3, ind3, color3);
@@ -108,6 +133,7 @@ function buildGeometry() {
 		theta = (2*Math.PI)*i/sectors
 		vert4[stacks*i+k] = [(ray*Math.cos(phi)*Math.cos(theta)),ray*Math.sin(phi),(ray*Math.cos(phi)*Math.sin(theta))];
 		norm4[stacks*i+k] = [(ray*Math.cos(phi)*Math.cos(theta)),ray*Math.sin(phi),(ray*Math.cos(phi)*Math.sin(theta))];
+		norm4[stacks*i+k] = normalize(norm4[stacks*i+k]);
 	}
 	j=0
 	// Top lateral area
@@ -128,6 +154,13 @@ function buildGeometry() {
 		ind4[j+2] = (i < sectors-1 ) ? (i+2)*stacks-1 : stacks-1
 		j+=3
 	}
-
 	addMesh(vert4, norm4, ind4, color4);
+}
+
+
+
+function normalize(v){
+	var len = Math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
+	var out = len > 0 ? [v[0]/len, v[1]/len, v[2]/len] : v;
+	return out;
 }
